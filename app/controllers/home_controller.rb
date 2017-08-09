85dfccf40193d0f61b948cd861641d100675563b
changmin <hellokiii@naver.com>
2017-08-09 10:25:47 +0900

    ajax

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  	@all_post = Post.all.reverse
+  end
+
+  def create
+  	Post.create(title: params[:title], content: params[:content])
+  	redirect_to '/home/index'
+  end
+
+  def reply_create
+  	Reply.create(content: params[:content], post_id: params[:id_of_post])
+  	redirect_to '/home/index'
+  end
+end
