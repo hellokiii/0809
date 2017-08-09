60cbf48f5304343711b9434f115db2fb6912e4d2
changmin <hellokiii@naver.com>
2017-08-09 14:32:50 +0900

    route, button 수정

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post 'home/create'
   post 'home/reply_create'
   get 'home/jsjs'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
